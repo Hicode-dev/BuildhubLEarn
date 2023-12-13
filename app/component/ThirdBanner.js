754198df7ba4acdc09e9b9b86afb9ea8c9cb6161
dtechoracle <iamezekieljeremiah@gmail.com>
2023-12-13 11:47:53 -0800

    Added some changes

--- app/component/ThirdBanner.js
+++ app/component/ThirdBanner.js
@@ -18,7 +18,7 @@ const ThirdBanner = () => {
      data-aos-offset="300"
      data-aos-easing="ease-in-sine" className='max-w-[400px]'>
                     <h1 className='text-[32px] max-w-[393px] font-[600]'>Helping you focus on what really matters</h1>
-                    <p className='my-3'>At buildhubb, we help create a platform for seamless Collaboration between frontend developers, backend developers, UI/UX designers and other fields of tech, that allows them to come together with the aim of building live and impactful projects to elevate their potfolios and boost their skillsets!</p>      
+                    <p className='my-3'>At buildhubb, we help create a platform for seamless Collaboration between frontend developers, backend developers, UI/UX designers and other fields of tech, that allows them to come together with the aim of building live and impactful projects to elevate their portfolios and boost their skillsets!</p>      
                 </div>
                 <div className='grid md:grid-cols-2 gap-2 mt-3 max-w-[470px]'>
                 <div className='flex gap-2 items-center '><AiOutlineCheck  className='dark:text-white'/> <div>Smooth Collaboration</div></div>
